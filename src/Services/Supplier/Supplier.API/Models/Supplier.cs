9752724a12c2d991cf8e9271a7774be540913c0c
Laya <l.salarhosseini@yahoo.com>
2025-07-23 10:25:37 -0400

    Add Supplier Service

---
+++ src/Services/Supplier/Supplier.API/Models/Supplier.cs
@@ -0,0 +1,17 @@
+﻿namespace Supplier.API.Models
+{
+    public class Supplier
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string ContactPerson { get; set; }=default!;
+        public string PhoneNumber { get; set; }=default!;
+        public string Email { get; set; } = default!;
+        public string Address { get; set; } = default!;
+        public string EconomicID { get; set; } = default!;
+        public bool IsActive { get; set; }
+        public int Rate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
